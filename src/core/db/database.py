3b8bee4a8799cd004f41a8ee879369632b751f99
Nam Nguyen <nam.nguyen23@sotatek.com>
2026-03-19 15:22:23 +0700

    config alembic

--- src/core/db/database.py
+++ src/core/db/database.py
@@ -3,12 +3,23 @@ from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from sqlalchemy import MetaData
 
 from src.core.config import settings
 
+POSTGRES_INDEXES_NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION)
+
 
 class Base(DeclarativeBase, MappedAsDataclass):
-    pass
+    metadata = metadata
 
 
 DATABASE_URI = settings.POSTGRES_URI
